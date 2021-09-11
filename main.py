5fff5ba3b69517ee69c703df4f9af95f09446f85
Thinh Hoang <thinhhoang@MacBook-Pro-de-Thinh.local>
2021-09-11 11:57:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from ngsim_processor import NGSIM_Processor as NGSIM
+
+if __name__ == '__main__':
+    container_t = []
+    container_x = []
+    container_y = []
+    num_of_trajs = 200
+
+    df = pd.read_csv('kalmaned2.csv')
+    ngsim = NGSIM(df)
+    ngsim.set_patch(0.0, 75.0, 500.0, 750.0) # see the notebook df_stats.py to see where these numbers come from
+
+    for i in range(num_of_trajs):
+        t, x, y = ngsim.get_trajectory_random()
+        container_t.append(t)
+        container_x.append(x)
+        container_y.append(y)
+        print('Trajectory {:d} generated'.format(i))
+
+    print('Process completed')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ ngsim_processor.py
@@ -0,0 +1,88 @@
+import random
+
+import pandas as pd
+
+
+class NGSIM_Processor:
+    def __init__(self, df):
+        print('NGSIM Processor is initialized')
+        self.x_min = 0
+        self.x_max = 0
+        self.y_min = 0
+        self.y_max = 0
+        self.df = df
+        self.max_trace_steps = 200
+
+    def set_patch(self, x_min, x_max, y_min, y_max):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
+
+    def get_df_trajs_in_patch(self):
+        return self.df[(self.df['Local_X'] > self.x_min) &
+                       (self.df['Local_X'] < self.x_max) &
+                       (self.df['Local_Y'] > self.y_min) &
+                       (self.df['Local_Y'] < self.y_max)]
+
+    def is_veh_in_patch(self, lx, ly):
+        if lx < self.x_min or lx > self.x_max or ly < self.y_min or ly > self.y_max:
+            return False
+        else:
+            return True
+
+    def get_trajectory_random(self):
+        df_patch_trajectories = self.get_df_trajs_in_patch()
+        num_of_rows = df_patch_trajectories.shape[0]
+        random_row_index = random.randrange(0, num_of_rows - 1)
+        vehicle_id_of_the_random_row = df_patch_trajectories.iloc[random_row_index].Vehicle_ID
+        gtime_at_random_row = df_patch_trajectories.iloc[random_row_index].Global_Time
+        # State variables for saving the trajectory
+        traj_x = []
+        traj_y = []
+        traj_t = []
+
+        gtime = gtime_at_random_row
+        # Trace backward in time until the vehicle is out of the patch
+        while True:
+            df_query = self.df[(self.df['Vehicle_ID'] == vehicle_id_of_the_random_row) & (self.df['Global_Time'] == gtime)]
+            if df_query.shape[0] == 0:
+                break
+            row_of_interest = df_query.iloc[0]
+            lx = row_of_interest.Local_X
+            ly = row_of_interest.Local_Y
+            if not self.is_veh_in_patch(lx, ly):
+                break
+            else:
+                traj_x.append(lx)
+                traj_y.append(ly)
+                traj_t.append(gtime)
+                gtime -= 100
+
+        gtime = gtime_at_random_row
+        # Trace forward in time until the vehicle is out of the patch
+        while True:
+            df_query = self.df[
+                (self.df['Vehicle_ID'] == vehicle_id_of_the_random_row) & (self.df['Global_Time'] == gtime)]
+            if df_query.shape[0] == 0:
+                break
+            row_of_interest = df_query.iloc[0]
+            lx = row_of_interest.Local_X
+            ly = row_of_interest.Local_Y
+            if not self.is_veh_in_patch(lx, ly):
+                break
+            else:
+                traj_x.append(lx)
+                traj_y.append(ly)
+                traj_t.append(gtime)
+                gtime += 100
+
+        # Perform a final sort
+        traj_x = [x for _, x in sorted(zip(traj_t, traj_x))]
+        traj_y = [x for _, x in sorted(zip(traj_t, traj_y))]
+        traj_t.sort()
+
+        print("Trajectory length " + str(len(traj_t)) + " generated")
+
+        return traj_t, traj_x, traj_y
+
