1389380a20aa9d188895f9a90d95fae3268e07a7
Thinh Hoang <dinhthinh.hoang@nxp.com>
2021-11-14 22:56:04 +0100

    HMM1

--- ngsim_processor.py
+++ ngsim_processor.py
@@ -45,6 +45,7 @@ class NGSIM_Processor:
         traj_x = []
         traj_y = []
         traj_t = []
+        traj_v = []
 
         gtime = gtime_at_random_row
         # Trace backward in time until the vehicle is out of the patch
@@ -55,12 +56,14 @@ class NGSIM_Processor:
             row_of_interest = df_query.iloc[0]
             lx = row_of_interest.Local_X
             ly = row_of_interest.Local_Y
+            v = row_of_interest.Vehicle_EKF_Velocity
             if not self.is_veh_in_patch(lx, ly):
                 break
             else:
                 traj_x.append(lx)
                 traj_y.append(ly)
                 traj_t.append(gtime)
+                traj_v.append(v)
                 gtime -= 100
 
         gtime = gtime_at_random_row
@@ -74,19 +77,86 @@ class NGSIM_Processor:
             row_of_interest = df_query.iloc[0]
             lx = row_of_interest.Local_X
             ly = row_of_interest.Local_Y
+            v = row_of_interest.Vehicle_EKF_Velocity
             if not self.is_veh_in_patch(lx, ly):
                 break
             else:
                 traj_x.append(lx)
                 traj_y.append(ly)
                 traj_t.append(gtime)
+                traj_v.append(v)
 
         # Perform a final sort
         traj_x = [x for _, x in sorted(zip(traj_t, traj_x))]
         traj_y = [x for _, x in sorted(zip(traj_t, traj_y))]
+        traj_v = [x for _, x in sorted(zip(traj_t, traj_v))]
         traj_t.sort()
 
         # print("Trajectory length " + str(len(traj_t)) + " generated")
 
         return traj_t, traj_x, traj_y
 
+    def get_trajectory_random_in_patch_with_velocity(self):
+            """
+            To return part of the trajectory where a random vehicle stays in the predefined road patch
+            :return: traj_t, traj_x, traj_y
+            """
+            df_patch_trajectories = self.get_df_trajs_in_patch()
+            num_of_rows = df_patch_trajectories.shape[0]
+            random_row_index = random.randrange(0, num_of_rows - 1)
+            vehicle_id_of_the_random_row = df_patch_trajectories.iloc[random_row_index].Vehicle_ID
+            gtime_at_random_row = df_patch_trajectories.iloc[random_row_index].Global_Time
+            # State variables for saving the trajectory
+            traj_x = []
+            traj_y = []
+            traj_t = []
+            traj_v = []
+
+            gtime = gtime_at_random_row
+            # Trace backward in time until the vehicle is out of the patch
+            while True:
+                df_query = self.df[(self.df['Vehicle_ID'] == vehicle_id_of_the_random_row) & (self.df['Global_Time'] == gtime)]
+                if df_query.shape[0] == 0:
+                    break
+                row_of_interest = df_query.iloc[0]
+                lx = row_of_interest.Local_X
+                ly = row_of_interest.Local_Y
+                v = row_of_interest.Vehicle_EKF_Velocity
+                if not self.is_veh_in_patch(lx, ly):
+                    break
+                else:
+                    traj_x.append(lx)
+                    traj_y.append(ly)
+                    traj_t.append(gtime)
+                    traj_v.append(v)
+                    gtime -= 100
+
+            gtime = gtime_at_random_row
+            # Trace forward in time until the vehicle is out of the patch
+            while True:
+                gtime += 100
+                df_query = self.df[
+                    (self.df['Vehicle_ID'] == vehicle_id_of_the_random_row) & (self.df['Global_Time'] == gtime)]
+                if df_query.shape[0] == 0:
+                    break
+                row_of_interest = df_query.iloc[0]
+                lx = row_of_interest.Local_X
+                ly = row_of_interest.Local_Y
+                v = row_of_interest.Vehicle_EKF_Velocity
+                if not self.is_veh_in_patch(lx, ly):
+                    break
+                else:
+                    traj_x.append(lx)
+                    traj_y.append(ly)
+                    traj_t.append(gtime)
+                    traj_v.append(v)
+
+            # Perform a final sort
+            traj_x = [x for _, x in sorted(zip(traj_t, traj_x))]
+            traj_y = [x for _, x in sorted(zip(traj_t, traj_y))]
+            traj_v = [x for _, x in sorted(zip(traj_t, traj_v))]
+            traj_t.sort()
+
+            # print("Trajectory length " + str(len(traj_t)) + " generated")
+
+            return traj_t, traj_x, traj_y, traj_v
